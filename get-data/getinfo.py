b0c9ebba91e48e6cf170cc317c282a16b8dc19a1
ron93 <ronmkare1@gmail.com>
2016-07-29 07:08:31 +0300

    get id

--- get-data/getinfo.py
+++ get-data/getinfo.py
@@ -1,3 +1,12 @@
-data = raw_input("Enter your id" "\n")
-file = open("MOCK_DATA.json" + "r+")
-if data in file.readline():
+import json
+user_input = raw_input("Enter id")
+data = open("MOCK_DATA.json" "r")
+
+def check():
+    if isinstance(user_input,int):
+        with data as "x"
+
+            temp_data = json.load("x")
+    for i in temp_data:
+        if i.get(id) == user_input
+
