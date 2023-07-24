5862ae33eb86a93cbf1d8fc7fb2659fdba9149a0
ron93 <ronmkare1@gmail.com>
2023-07-25 01:32:47 +0300

    Revert "gcp terraform infrastructure files"
    This reverts commit 8babaf195ad376bccd74a16bd1e952956633ebab.

--- terraform-gcp/output.tf
+++
@@ -1,5 +0,0 @@
-# designates data to display when `terraform apply` is called
-
-# output "ip" {
-#   value = google_compute_instance.vm_instance.network_interface.0.network_ip
-# }
--- terraform-gcp/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-project          = "fourth-library-393410"
-credentials_file = "fourth-library-393410-9565d5192f34.json"
