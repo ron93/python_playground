8babaf195ad376bccd74a16bd1e952956633ebab
ron93 <ronmkare1@gmail.com>
2023-07-24 23:25:48 +0300

    gcp terraform infrastructure files

---
+++ terraform-gcp/output.tf
@@ -0,0 +1,5 @@
+# designates data to display when `terraform apply` is called
+
+# output "ip" {
+#   value = google_compute_instance.vm_instance.network_interface.0.network_ip
+# }
---
+++ terraform-gcp/terraform.tfvars
@@ -0,0 +1,2 @@
+project          = "fourth-library-393410"
+credentials_file = "fourth-library-393410-9565d5192f34.json"
