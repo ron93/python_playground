ec8ce8394130d7cd8b5b00bd8773b46370537b41
ron93 <ronmkare1@gmail.com>
2023-12-16 12:10:03 +0300

    update: middleware function to modify API request

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 # /app.py
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import StreamingResponse
 
 app = FastAPI()
+
+# middleware function
+@app.middleware("http")
+async  def modify_request_response_middleware(request: Request, call_next):
+    # intercept and  modify the incoming request
+    request.scope["path"] = str(request.url.path).replace("api", "apiv2")
+    # process modified request
+    response = await call_next(request)
+    # transform the outgoing response
+    if isinstance(response, StreamingResponse):
+        response.headers["X-Custom-Header"] = "Modified"
+    return response
 
 @app.get("/info")
 async def hello():
