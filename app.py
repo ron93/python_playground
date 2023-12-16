3b6478f55ed6f764b1c3dbbe6528021a9b267c21
ron93 <ronmkare1@gmail.com>
2023-12-16 16:46:23 +0300

    update: middleware class to limit rate of API calls

--- app.py
+++ app.py
@@ -1,6 +1,49 @@
 # /app.py
 from fastapi import FastAPI, Request
-from fastapi.responses import StreamingResponse
+from fastapi.responses import StreamingResponse, JSONResponse
+from starlette.middleware.base import BaseHTTPMiddleware
+from datetime import datetime, timedelta
+
+# rate limiting
+class RateLimitingMiddleware(BaseHTTPMiddleware):
+    # rate limiting configs
+    RATE_LIMIT_DURATION = timedelta(minutes=1)
+    RATE_LIMIT_REQUESTS = 3
+
+    def __init__(self, app):
+        super().__init__(app)
+        # Dict to store request counts for each IP
+        self.request_counts = {}
+
+    async def dispatch(self, request, call_next):
+        # get client IP
+        client_ip = request.client.host
+
+        # check if IP is already preswnt in request_counts
+        request_count, last_request = self.request_counts.get(client_ip, (0, datetime.min))
+
+        # calculate time elapsed since last request
+        elapsed_time = datetime.now() - last_request
+
+        if elapsed_time  > self.RATE_LIMIT_DURATION:
+            # if elapsed time is greater than the rate limit duration, reset the count
+            request_count = 1
+        else:
+            if request_count >= self.RATE_LIMIT_REQUESTS:
+                # If the request count exceeds the rate limit, return a JSON response with an error message
+                return JSONResponse(
+                    status_code=429,
+                    content={"message": "Rate limit exceeded. Please try again later."}
+                )
+            request_count +=1
+
+        # update the request count and last request timestatmp for the IP
+        self.request_counts[client_ip] = (request_count, datetime.now())
+
+        # proceed with request
+        response = await call_next(request)
+
+        return response
 
 app = FastAPI()
 
@@ -15,6 +58,8 @@ async  def modify_request_response_middleware(request: Request, call_next):
     if isinstance(response, StreamingResponse):
         response.headers["X-Custom-Header"] = "Modified"
     return response
+
+app.add_middleware(RateLimitingMiddleware)
 
 @app.get("/info")
 async def hello():
