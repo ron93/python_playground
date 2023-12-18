7aa685910cf77b11930fda24014baa787c357a70
ron93 <ronmkare1@gmail.com>
2023-12-18 15:56:30 +0300

    QLabel

---
+++ basic/widgets_1.py
@@ -0,0 +1,30 @@
+import sys
+
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QApplication, QLabel, QMainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+
+        widget = QLabel("Hello")
+        # get current font from desktop
+        font = widget.font()
+        font.setPointSize(30)
+        # set widget font
+        widget.setFont(font)
+        # flags for horizontal alighnement : Qt.AlignLeft, Qt.AlignRight, Qt.AlignHCenter, Qt.AlignJustify
+        # flags for vertical alignment : Qt.AlignTop, Qt.AlignBottom, Qt.AlignVCenter
+        # flag for alignment in both directions : Qt.AlignCenter
+        # combining flags i.e horizontal_alignment | vertical_alignment
+        widget.setAlignment(Qt.AlignCenter | Qt.AlignVCenter)
+
+        self.setCentralWidget(widget)
+ 
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+
+app.exec()        
