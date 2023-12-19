c4b4138f7843407752857c8d05322dab720c5fbe
ron93 <ronmkare1@gmail.com>
2023-12-19 13:07:46 +0300

    QListWidget

---
+++ basic/widget_5.py
@@ -0,0 +1,29 @@
+import sys
+from PySide6.QtWidgets import QApplication, QListWidget, QMainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+
+        widget = QListWidget()
+        widget.addItems(["One", "Two", "Three"])
+
+        widget.currentItemChanged.connect(self.index_changed)
+        widget.currentTextChanged.connect(self.text_changed)
+
+        self.setCentralWidget(widget)
+
+    def index_changed(self, i):
+        print(i.text())
+
+    def text_changed(self, s):
+        print(s)
+
+app= QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
