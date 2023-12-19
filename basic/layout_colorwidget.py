0356e0fa386b4d32ace0e57efd3d21cc95593486
ron93 <ronmkare1@gmail.com>
2023-12-19 14:37:58 +0300

    custom widget -- Color

---
+++ basic/layout_colorwidget.py
@@ -0,0 +1,16 @@
+from typing import Optional
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QColor, QPalette
+from PySide6.QtWidgets import QWidget
+
+# custom widget Color
+class Color(QWidget):
+    def __init__(self, color):
+        super().__init__()
+        self.setAutoFillBackground(True)
+
+        palette = self.palette()
+        palette.setColor(QPalette.Window, QColor(color))
+        self.setPalette(palette)
+
+        
