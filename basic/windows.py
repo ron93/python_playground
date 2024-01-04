ba074fd1f553e0e0896275d05311f7eb36435492
ron93 <ronmkare1@gmail.com>
2024-01-04 13:55:42 +0300

    windows -- fix:block new window if existing

--- basic/windows.py
+++ basic/windows.py
@@ -1,4 +1,5 @@
 import sys
+from random import randint
 
 from PySide6.QtWidgets import (
     QApplication,
@@ -18,20 +19,23 @@ class AnotherWindow(QWidget):
     def __init__(self):
         super().__init__()
         layout = QVBoxLayout()
-        self.label = QLabel("Another Window")
+        self.label = QLabel("Another Window %d" % randint(0, 100))
         layout.addWidget(self.label)
         self.setLayout(layout)
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.w = None # no external window  created
+
         self.button = QPushButton("Push for Window")
         self.button.clicked.connect(self.show_new_window)
         self.setCentralWidget(self.button)
 
     def show_new_window(self,checked):
-        self.w = AnotherWindow()
-        self.w.show()
+        if self.w is None:
+            self.w = AnotherWindow()
+            self.w.show()
 
 app = QApplication(sys.argv)
 w = MainWindow()
