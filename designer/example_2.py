0a13c007d52b50d887288abd3a5831835e578d72
ron93 <ronmkare1@gmail.com>
2024-01-06 14:39:22 +0300

    designer -- example_2

---
+++ designer/example_2.py
@@ -0,0 +1,18 @@
+import  os 
+import sys
+
+from PySide6 import QtWidgets
+from PySide6.QtUiTools import QUiLoader
+
+loader = QUiLoader()
+basedir = os.path.dirname(__file__)
+
+# custom setup function
+def mainwindow_setup(w):
+    w.setWindowTitle("MainWindow Title")
+
+app = QtWidgets.QApplication(sys.argv)
+window = loader.load(os.path.join(basedir, "mainwindow.ui"), None)
+mainwindow_setup(window)
+window.show()
+app.exec()
