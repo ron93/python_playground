d9bceb0dbbfe4e775c1db4c7932c7bd63976c8f2
ron93 <ronmkare1@gmail.com>
2024-01-20 12:52:12 +0300

    encode domain name

--- part_1.py
+++ part_1.py
@@ -10,4 +10,26 @@ class DNSHeader:
     num_answers: int = 0 
     num_authorities: int = 0
     num_additionals: int = 0
-    
+
+@dataclass
+class DNSQuestion:
+    name: bytes
+    # *type_ -> because type is a reseved  class
+    type_: int
+    class_: int
+
+# ! before string tells python to use the byte order for computer networking
+    # byte order -> big and  small endian
+def header_to_bytes(header):
+    fields = dataclasses.astuple(header)
+    # 6 h's because there are 6 fields
+    return struct.pack("!HHHHHH", *fields)
+
+def question_to_bytes(question):
+    return question.name  + struct.pack("!HH", question.type_, question.class_)
+
+def encode_dns(domain_name):
+    encode = b""
+    for part in domain_name.encode("ascii").split(b"."):
+        encode += bytes([len(part)]) + part
+    return encode + b"\x00"
